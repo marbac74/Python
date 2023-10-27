502e7746c0fe30464c5b5125f14bf2a4c5c1a52e
Marco Bachini <marbac74@hotmail.com>
2023-10-27 18:13:19 +0200

    modificato dizionari.py

--- dizionari.py
+++ dizionari.py
@@ -7,3 +7,27 @@ def istogramma(stringa):
 def stampa_isto(isto):
     for item in isto:
         print(item, isto[item])
+
+def lookup_inverso(dict, value):
+    for key in dict:
+        if dict[key] == value:
+            return key
+    raise LookupError('Il valore non compare nel dizionario')
+
+def inverti_dict(diz):
+    inverso = dict()
+    for chiave in diz:
+        valore = diz[chiave]
+        if valore not in inverso:
+            inverso[valore] = [chiave]
+        else:
+            inverso[valore].append(chiave)
+    return inverso
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
