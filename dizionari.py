3b2cc1a49e00fc607031eeb1f8727bf52ccacb82
Marco Bachini <marbac74@hotmail.com>
2023-10-30 16:20:53 +0100

    aggiunte funzioni rotazioni e coppie_ruotabili

--- dizionari.py
+++ dizionari.py
@@ -1,5 +1,7 @@
 import time
 
+from minuscole import ruota_parola
+
 def istogramma(stringa):
     d = dict()
     for c in stringa:
@@ -42,13 +44,13 @@ def memo_fibonacci(n):
     memo[n] = res
     return res
 
-start = time.time()
+"""start = time.time()
 fibonacci(42)
 print("Per calcolare fibonacci(42) sono passati ", time.time() - start, " secondi") 
 
 second_start = time.time()
 memo_fibonacci(42)
-print("Per calcolare memo_fibonacci(42) sono passati ", time.time() - second_start, " secondi")
+print("Per calcolare memo_fibonacci(42) sono passati ", time.time() - second_start, " secondi")"""
 
 def controlla(stringa):
     inputfile = open('words.txt')
@@ -60,3 +62,31 @@ def controlla(stringa):
         return True
     else:
         return False
+    
+def ha_duplicati(lista):
+    incontrati = {}
+    for elemento in lista:
+        if elemento in incontrati:
+            return True
+        incontrati[elemento] = 'inserted'
+    return False
+
+def rotazioni(parola):
+    """La funzione ruotabili prende una stringa come input
+    e produce come output un dizionario contenente tutte le
+    possibili stringhe generate dalla rotazione della stringa
+    per un intero n compreso tra 1 e 25"""
+    rotazioni = {}
+    for numero in range(1, 26):
+        rotazioni[ruota_parola(parola, numero)] = True
+    return rotazioni.keys()
+
+def coppie_ruotabili(elenco):
+    diz = {}
+    for parola1 in elenco:
+        lista = []
+        for parola2 in elenco:
+            if parola2 in rotazioni(parola1):
+                lista.append(parola2)
+                diz[parola1] = lista
+    return diz
